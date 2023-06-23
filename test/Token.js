8cfcaefa137ac4042989d450e505d9f651c7849b
mehdinejjar86 <shiverstalker@gmail.com>
2023-06-23 18:24:09 +0200

    Token Approval

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-  let token, accounts, deployer, receiver
+  let token, accounts, deployer, receiver, exchange
   const name = 'Night^^Stalker'
   const symbol = 'N^^S'
   const decimals = '18'
@@ -18,6 +18,7 @@ describe('Token', () => {
     accounts = await ethers.getSigners()
     deployer = accounts[0]
     receiver = accounts[1]
+    exchange = accounts[2]
   })
 
   describe('Deployment', () => {
@@ -76,5 +77,37 @@ describe('Token', () => {
       })
     })
 
+  })
+
+  describe('Approving Tokens', () => {
+    let amount, transaction, result
+
+    beforeEach(async () => {
+      amount = tokens(100)
+      transaction = await token.connect(deployer).approve(exchange.address, amount)
+      result = await transaction.wait()
+    })
+
+    describe('Success', () => {
+      it('Allocates and allowance for delegated token spending', async () => {
+        expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+      })
+
+      it('Emits an approval event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Approval')
+        const args = event.args
+        expect(args.owner).to.equal(deployer.address)
+        expect(args.spender).to.equal(exchange.address)
+        expect(args.value).to.equal(amount)
+      })
+    })
+
+    describe('Failure', () => {
+      it('Rejects Invalid spenders', async () => {
+        await expect(token.connect(deployer).approve("0x0000000000000000000000000000000000000000", tokens(100))).to.be.reverted
+        
+      })
+    })
   })
 })
